7ad19a95b8fb9d2c9beb6c48c1b82fd13bf11fc9
delhierroantonio <delhierroantonio@icloud.com>
2023-06-01 09:08:52 -0600

    feat: :construction: implement nextauth and create .env file
    Implement nextauth in order to add user anthentication to the app as well as create the .env file with the corresponding client ID and secret keys

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,25 @@
-// import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import { useSession, signIn, signOut } from "next-auth/react";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return (
-    <main className='bg-cyan-900 w-screen h-screen flex justify-center items-center'>
-      <section className='text-center w-full'>
-        <button className=' bg-slate-200 py-2 px-6 rounded-md'>Login with Google</button>
-      </section>
-    </main>
-  )
+	const { data: session } = useSession();
+	if (!session) {
+		return (
+			<main className="bg-cyan-900 w-screen h-screen flex justify-center items-center">
+				<section className="text-center w-full">
+					<button onClick={() => signIn('google')} className=" bg-slate-200 py-2 px-6 rounded-md">
+						Login with Google
+					</button>
+				</section>
+			</main>
+		);
+	}
+	return (
+		<div>
+			Signed in as {session.user.email} <br />{" "}
+			<button onClick={() => signOut()} className="py-2 px-4 bg-slate-200 rounded-md">Sign out</button>
+		</div>
+	);
 }
